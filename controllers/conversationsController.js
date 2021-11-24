1a888889958c4267911dc9f503101676da6010ee
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-11-23 19:46:16 -0700

    Added controller path to show messages on conversation show page

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -24,6 +24,32 @@ const conversationShow = (req, res) => {
     })
 }
 
+
+// /conversations/:id
+// This will find the conversation and find all messages that have that conversations id
+// Is it possible to do res1 and res2 in this way?
+// How else can you go about doing two queries within one API call?
+// const messageShow = (req, res1, res2) => {
+//     db.Conversation.findById(req.params.id, (err, foundConversation) => {
+//         if (err) console.log('Error in conversation show:', err)
+
+//         if(!foundConversation) return res1.json({
+//             message: 'No Conversation found in database.'
+//         })
+
+//         res1.status(200).json({ conversation: foundConversation});
+//     })
+//     db.Message.find({/*filter for all messages that have req.params.id*/}, (err, foundMessages) => {
+//         if (err) console.log('Error in message show:', err)
+
+//         if(!foundMessages) return res2.json({
+//             message: 'No meessages found in database.'
+//         })
+
+//         res2.status(200).json({ messages: foundMessages});
+//     })
+// }
+
 module.exports = {
     conversationIndex,
     conversationShow,
