cce2811be57edebf7a97755662b9c2a8489d7855
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-11-23 20:13:34 -0700

    Formatting changes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,17 +3,17 @@ const express = require('express');
 const db = require('../models');
 
 // Account Index will be used for search functionality
-const accountIndex = (req, res) => {
-    db.User.find({}, (err, foundUsers) => {
-        if (err) console.log('Error in user index:', err)
+// const accountIndex = (req, res) => {
+//     db.User.find({}, (err, foundUsers) => {
+//         if (err) console.log('Error in user index:', err)
 
-        if(!foundUsers) return res.json({
-            message: 'No Users found in database.'
-        })
+//         if(!foundUsers) return res.json({
+//             message: 'No Users found in database.'
+//         })
 
-        res.status(200).json({ users: foundUsers});
-    })
-}
+//         res.status(200).json({ users: foundUsers});
+//     })
+// }
 
 const accountShow = (req, res) => {
     db.User.findById(req.params.id, (err, foundUser) => {
@@ -28,33 +28,34 @@ const accountShow = (req, res) => {
 };
 
 // Update Page
-const accountUpdate = (req, res) => {
-    db.User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
-        if (err) {
-            console.log('Error in user#update:', err)
+// const accountUpdate = (req, res) => {
+//     db.User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
+//         if (err) {
+//             console.log('Error in user#update:', err)
     
-            return res.send("Incomplete user#update controller function");
-        }
+//             return res.send("Incomplete user#update controller function");
+//         }
 
-        res.status(200).json({
-            updatedUser
-        });
-    });
-};
+//         res.status(200).json({
+//             updatedUser
+//         });
+//     });
+// };
 
 // Create Account
-const accountCreate = (req, res) => {
-    db.User.create(req.body, (err, savedUser) => {
-        if (err) console.log('Error in user#create:', err)
+// const accountCreate = (req, res) => {
+//     db.User.create(req.body, (err, savedUser) => {
+//         if (err) console.log('Error in user#create:', err)
 
-        // Validations and error handling here
+//         // Validations and error handling here
 
-        res.status(201).json({ user: savedUser })
-    })
-}
+//         res.status(201).json({ user: savedUser })
+//     })
+// }
 
 module.exports = {
+    // accountIndex,
     accountShow,
-    accountUpdate,
-    accountCreate,
+    // accountUpdate,
+    // accountCreate,
 };
